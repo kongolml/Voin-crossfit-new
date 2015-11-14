2bb5386f3286199e00e7762940ec54ba7d36d600
Konstantin Golosov <Missenot@MacBook-Pro-Nasta.local>
2015-11-14 19:55:02 +0200

    almost done

--- js/main.js
+++ js/main.js
@@ -1,13 +1,27 @@
-$(window).scroll(function(){
+var owl = $('.owl-carousel');
+var imagesCarousel = $('.owl-carousel.images');
+var textCarousel = $('.owl-carousel.text');
 
-	if( $(window).scrollTop()>=$("section").height() ){
-		$(".info").removeClass("not-shown");
-		$(".info").addClass("shown");
-	} else {
-		if( $(".info").hasClass("shown") ){
-			$(".info").addClass("not-shown");
-			$(".info").removeClass("shown");
-		}
-	}
+owl.owlCarousel({
+	loop:false,
+	margin:0,
+	nav:false,
+	dots: true,
+	items:1,
+	/*mouseDrag: false,
+	touchDrag: false,
+	pullDrag:false,
+	freeDrag:false,*/
+	autoplay: true,
+	autoplayTimeout: 3000,
+	animateOut: 'fadeOut',
+	animateIn: 'fadeIn'
+});
 
-})
+imagesCarousel.on('changed.owl.carousel', function(event) {
+	textCarousel.trigger('to.owl.carousel', event.item.index)
+});
+
+textCarousel.on('changed.owl.carousel', function(event) {
+	imagesCarousel.trigger('to.owl.carousel', event.item.index)
+});
